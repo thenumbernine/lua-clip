b326422a390ecc1470e51ea6f11edb5870bb7e25
Christopher Moore <christopher.e.moore@gmail.com>
2024-09-16 18:41:53 -0700

    added pasting images

--- clip.lua
+++ clip.lua
@@ -14,14 +14,14 @@ local textFormat = clip.clip_text_format()
 local function text(...)
 	local result
 	local lock = clip.clip_lock_new()
-	local iscopying = select('#', ...) > 0
+	local copying = select('#', ...) > 0
 	local tocopy = ...
 	assert(xpcall(function()
-		if iscopying then 	-- setting clipboard
+		if copying then 	-- setting clipboard
 			tocopy = tostring(tocopy)
 			-- TODO do we need null term?
-			assert(clip.clip_lock_clear(lock))
-			assert(clip.clip_lock_set_data(lock, textFormat, ffi.cast('char const*', tocopy), #tocopy))
+			assert(clip.clip_lock_clear(lock), 'clipboard clear failed')
+			assert(clip.clip_lock_set_data(lock, textFormat, ffi.cast('char const*', tocopy), #tocopy), 'clipboard set text failed')
 		else	-- returning clipboard
 			if clip.clip_lock_is_convertible(lock, textFormat) then
 				local len = clip.clip_lock_get_data_length(lock, textFormat)
@@ -41,27 +41,42 @@ local function text(...)
 end
 
 local imageFormat = clip.clip_image_format()
-local function image()
+local function image(...)
 	local result
 	local lock = clip.clip_lock_new()
-	assert(xpcall(function()	
-		if clip.clip_lock_is_convertible(lock, imageFormat) then
-			local clipImage = clip.clip_image_new()
-			if clip.clip_lock_get_image(lock, clipImage) then
-				local spec = clip.clip_image_spec(clipImage)
-				asserteq(spec.bits_per_pixel, 32, 'clipboard bpp')
-				result = Image(tonumber(spec.width), tonumber(spec.height), tonumber(bit.rshift(spec.bits_per_pixel, 3)), 'unsigned char')
-				local dstRowSize = result.width * result.channels
-				local dstp = clip.clip_image_data(clipImage)
-				local srcp = result.buffer
-				assertne(dstp, nil, 'dstp')
-				for y=0,result.height-1 do
-					ffi.copy(srcp, dstp, dstRowSize)
-					srcp = srcp + dstRowSize
-					dstp = dstp + spec.bytes_per_row
-				end
+	local copying = select('#', ...) > 0
+	local tocopy = ...
+	assert(xpcall(function()
+		if copying then
+			assert(Image:isa(tocopy), "can't paste image, it's not an image")
+			assert(clip.clip_lock_clear(lock), "clipboard clear failed")
+			asserteq(ffi.sizeof(tocopy.format), 1, 'image.format')	-- ... right?
+			-- can only handle 32bpp images soooo ...
+			if tocopy.channels < 4 then
+				local blank = Image(tocopy.width, tocopy.height, 1, tocopy.format)
+				tocopy = tocopy:combine(table{blank}:rep(4 - tocopy.channels):unpack())
 			end
-			clip.clip_image_free(clipImage)
+			asserteq(tocopy.channels, 4, 'channels')
+			assert(clip.clip_lock_set_data(lock, imageFormat, tocopy.buffer, tocopy.width*tocopy.height*tocopy.channels*ffi.sizeof(tocopy.format)), "clipboard set image failed")
+		else
+			if clip.clip_lock_is_convertible(lock, imageFormat) then
+				local clipImage = clip.clip_image_new()
+				if clip.clip_lock_get_image(lock, clipImage) then
+					local spec = clip.clip_image_spec(clipImage)
+					asserteq(spec.bits_per_pixel, 32, 'clipboard bpp')
+					result = Image(tonumber(spec.width), tonumber(spec.height), tonumber(bit.rshift(spec.bits_per_pixel, 3)), 'unsigned char')
+					local dstRowSize = result.width * result.channels
+					local dstp = clip.clip_image_data(clipImage)
+					local srcp = result.buffer
+					assertne(dstp, nil, 'dstp')
+					for y=0,result.height-1 do
+						ffi.copy(srcp, dstp, dstRowSize)
+						srcp = srcp + dstRowSize
+						dstp = dstp + spec.bytes_per_row
+					end
+				end
+				clip.clip_image_free(clipImage)
+			end
 		end
 	end, errorHandler))
 	clip.clip_lock_free(lock)
